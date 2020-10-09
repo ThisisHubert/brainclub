450d88a6d85e00c118d17a0aa77a6dc3294ff5ea
wonjerry <wonjerry@squarelab.co>
2020-10-09 18:10:36 +0900

    Add header in home page

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -4,7 +4,6 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './components/App'
 import * as serviceWorker from './serviceWorker'
-import 'helpers/debugHelper'
 
 ReactDOM.render(
   <React.StrictMode>
--- web/src/pages/Home.tsx
+++ web/src/pages/Home.tsx
@@ -1,8 +1,50 @@
-import { Box } from '@material-ui/core'
 import React, { ReactElement } from 'react'
+import {
+  AppBar,
+  Box,
+  Button,
+  IconButton,
+  makeStyles,
+  Toolbar,
+  Typography,
+} from '@material-ui/core'
+import MenuIcon from '@material-ui/icons/Menu'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}))
 
 function Home(): ReactElement {
-  return <Box>Hello world</Box>
+  const classes = useStyles()
+  return (
+    <>
+      <AppBar position='static'>
+        <Toolbar>
+          <IconButton
+            edge='start'
+            className={classes.menuButton}
+            color='inherit'
+            aria-label='menu'
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant='h6' className={classes.title}>
+            Teamname_untitled
+          </Typography>
+          <Button color='inherit'>Join</Button>
+        </Toolbar>
+      </AppBar>
+      <Box> Hello world</Box>
+    </>
+  )
 }
 
 export default Home
